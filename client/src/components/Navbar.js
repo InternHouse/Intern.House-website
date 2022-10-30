ca4858319e57dba2daf05179d06050bcb24b9cfc
Cassidy Johnson <cassidyrose56@gmail.com>
2022-10-30 15:53:04 -0500

    configured style folder and added dependencies for sass

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import styles from "../styles";
-
+import '../styles/sitewide.scss';
+import '../styles/navbar.scss';
 
 class Navbar extends Component {
     render() {
         return(
-            <nav style={styles.navbar}>
-                <div style={styles.h1}>Intern.House</div>
-                <div style={styles.h2}>
-                    <div style={styles.h2}>Testimonials</div>
-                    <div style={styles.h2}>About</div>
-                    <div style={styles.h2}>Rent</div>
-                </div>
-
+            <nav id='navbar'>
+                <h1 id='title'>Intern.House</h1>
+                <section>
+                    <h2>Testimonials</h2>
+                    <h2>About</h2>
+                    <h2>Rent</h2>
+                </section>
             </nav>
         )
     }
